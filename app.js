2521991a48437d17171503aeb8e29db4d4f29a62
Eric Anspach <eric.j.anspach@gmail.com>
2018-03-25 22:02:54 -0400

    Add files via upload
    First commit for Budgety app

---
+++ app.js
@@ -0,0 +1,45 @@
+// Budget App for Complete JavaScript Course
+
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+    // code for budget module
+
+})();
+
+// UI CONTROLLER
+var UIController = (function() {
+    
+    // code for UI module
+    
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var ctrlAddItem = function() {
+        
+        // 1. Get the field input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+        console.log('It works!')
+        
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event) {
+       
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+        
+    }); 
+    
+    
+})(budgetController, UIController);
+
+
