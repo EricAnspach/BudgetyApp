da67d06ab05858e8075031e6ff3b5991565ee800
Eric Anspach <eric.j.anspach@gmail.com>
2018-04-07 23:19:26 -0400

    Final revisions to app

--- app.js
+++ app.js
@@ -183,6 +183,12 @@ var UIController = (function() {
         
     };
     
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+    
     return {
         getInput: function() {
             return {
@@ -260,12 +266,6 @@ var UIController = (function() {
             
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
             
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-            
             nodeListForEach(fields, function(current, index) {
                 
                 if (percentages[index] > 0) {
@@ -289,6 +289,20 @@ var UIController = (function() {
             
         },
         
+        changedType: function() {
+          
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+            
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');                
+            });
+            
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+            
+        },
     
         getDOMstrings: function() {
             return DOMstrings;
@@ -314,6 +328,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
         
     };
     
